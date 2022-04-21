22e26e416ae7c76479e51ce35faa10976fba7e0d
Anuradha <anupanduri@gmail.com>
2022-04-21 15:22:15 -0600

    fileupload.controller is modified

--- controllers/fileupload.controller.js
+++ controllers/fileupload.controller.js
@@ -1,5 +1,34 @@
-const databasefileupload=async(req,res)=>{
-    let countrydata=req.body.firstname;
-    console.log("the country data in nodejs",countrydata)
-}
-module.exports=databasefileupload;
+const { db } = require("../models/country.model");
+
+const databasefileupload = async (req, res) => {
+  countrydata = req.body.excelvalues;
+  console.log("the databasevalues", countrydata);
+  console.log(Object.entries(countrydata));
+  console.log(countrydata.length);
+  const headers = countrydata[0];
+ 
+    
+    const data = countrydata.slice(1, countrydata.length);
+ 
+    const jsoncountrydata = [],
+    countrydataarr= data,
+    tablehead = headers
+    for (let i=0; i<countrydataarr.length; i++) {
+             let details= countrydataarr[i],
+                    obj = {};
+            for (let j=0; j<tablehead.length; j++)
+                    obj[tablehead[j]] = details[j];
+            jsoncountrydata.push(obj);
+    }
+ console.log("countrydata",jsoncountrydata);
+
+ 
+  
+//    for (let i = 1; i < countrydata.length; i++) {
+//        console.log("the values are",(Object.assign({},countrydata[i])));
+
+ db.collection('country').insertMany(jsoncountrydata);
+  
+//    }
+};
+module.exports = databasefileupload;
